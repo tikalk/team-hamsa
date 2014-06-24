78fb7c76c03fc2af586ba9473fc1991528409db0
Adam Mesha <adam@tikalk.com>
2014-06-24 16:07:08 +0300

    Complete reorganization of the front end code

---
+++ app/js/config.js
@@ -0,0 +1,13 @@
+var require = {
+    deps: ['main'],
+    paths: {
+        backbone: '../bower_components/backbone/backbone',
+        jquery: '../bower_components/jquery/dist/jquery',
+        marionette: '../bower_components/marionette/lib/core/amd/backbone.marionette',
+        underscore: '../bower_components/lodash/dist/lodash.underscore',
+        'backbone.wreqr' : '../bower_components/backbone.wreqr/lib/amd/backbone.wreqr',
+        'backbone.eventbinder' : '../bower_components/backbone.eventbinder/lib/amd/backbone.eventbinder',
+        'backbone.babysitter' : '../bower_components/backbone.babysitter/lib/amd/backbone.babysitter',
+        templates: 'templates/compiled',
+    }
+};
